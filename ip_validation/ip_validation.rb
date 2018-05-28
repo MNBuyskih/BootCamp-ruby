47767848cea490df4c823f77dd64383e3150c754
Mihail Buyskih <mihail@mixbook.com>
2018-05-28 16:50:36 +0300

    Refactoring and tests naming convetions

--- ip_validation/ip_validation.rb
+++ ip_validation/ip_validation.rb
@@ -1,9 +1,7 @@
 require 'ipaddr'
 
-def ip_validation(str)
-  begin
-    IPAddr.new(str).ipv4?
-  rescue IPAddr::InvalidAddressError
-    false
-  end
+def ipv4?(str)
+  IPAddr.new(str).ipv4?
+rescue IPAddr::InvalidAddressError
+  false
 end
