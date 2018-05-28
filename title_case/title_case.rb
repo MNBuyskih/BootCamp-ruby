35ecc1cb24469f4bf59556d03332f3dff7b7d476
Mihail Buyskih <mihail@mixbook.com>
2018-05-28 17:18:32 +0300

    Small refactoring

--- title_case/title_case.rb
+++ title_case/title_case.rb
@@ -1,9 +1,8 @@
 def title_case(string, except = '')
-  except_list = except.downcase.split(' ')
-  w = string
-          .downcase
-          .split
-          .each_with_index
-          .map {|w, i| i > 0 && except_list.include?(w) ? w : w.capitalize}
-          .join(' ')
+  except_list = except.downcase.split
+  string
+    .capitalize
+    .split
+    .map { |w| except_list.include?(w) ? w : w.capitalize }
+    .join(' ')
 end
