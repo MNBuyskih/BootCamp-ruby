965cb5e1b0695706e669af1dfa687fab000cb82f
Mihail Buyskih <mihail@mixbook.com>
2018-05-28 16:55:56 +0300

    Fix spec messages

--- multiples_3_5/multiples_3_5_spec.rb
+++ multiples_3_5/multiples_3_5_spec.rb
@@ -1,8 +1,8 @@
 require 'rspec'
 require_relative 'multiples_3_5'
 
-describe 'multiples of 3 and 5' do
-  it '#multiples_3_5' do
+describe '#multiples_3_5' do
+  it 'returns sum of all multiples of 3 or 5' do
     expect(multiples_3_5(10)).to eq(23)
     expect(multiples_3_5(20)).to eq(78)
     expect(multiples_3_5(200)).to eq(9168)
