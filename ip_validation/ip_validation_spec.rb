2ef29bde1f47dfd406568c838b0b0654dbe42613
Mihail Buyskih <mihail@mixbook.com>
2018-05-28 17:00:40 +0300

    Fix spec messages

--- ip_validation/ip_validation_spec.rb
+++ ip_validation/ip_validation_spec.rb
@@ -2,15 +2,15 @@ require 'rspec'
 require_relative 'ip_validation'
 
 describe '#ipv4?' do
-  describe 'for valid addresses' do
-    it 'does returns true' do
+  context 'for valid addresses' do
+    it 'returns true' do
       expect(ipv4?('1.2.3.4')).to be_truthy
       expect(ipv4?('123.45.67.89')).to be_truthy
     end
   end
 
-  describe 'for invalid addresses' do
-    it 'does returns false' do
+  context 'for invalid addresses' do
+    it 'returns false' do
       expect(ipv4?('1.2.3')).to be_falsey
       expect(ipv4?('1.2.3.4.5')).to be_falsey
       expect(ipv4?('123.456.78.90')).to be_falsey
